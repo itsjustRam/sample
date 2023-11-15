79858db40dd84b777e1af34fcbdcd533d7fadbd8
itsjustRam <ramprashanth@student.tce.edu>
2023-11-15 05:45:44 +0530

    Eng and Hi

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sample';
+  constructor(private titleService: Title) { 
+    this.titleService.setTitle($localize`${this.title}`);
+  }
+}
